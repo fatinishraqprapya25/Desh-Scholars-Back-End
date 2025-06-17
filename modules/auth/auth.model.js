7624ee62575ca3cffce2a3b415898942d5100024
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-17 13:30:54 +0600

    basic validations for registering user changed

--- modules/auth/auth.model.js
+++ modules/auth/auth.model.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName: {
+    name: {
         type: String,
         required: true,
         message: "first name is required!"
-    },
-    lastName: {
-        type: String,
-        required: true,
     },
     email: {
         type: String,
--- modules/auth/auth.validations.js
+++ modules/auth/auth.validations.js
@@ -3,8 +3,7 @@ const { z } = require("zod");
 const authValidations = {};
 
 authValidations.registerValidation = z.object({
-    firstName: z.string().min(1, { message: "First name is required" }),
-    lastName: z.string().min(1, { message: "Last name is required" }),
+    name: z.string().min(1, { message: "First name is required" }),
     email: z.string().email({ message: "Invalid email format" }),
     password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
 });
