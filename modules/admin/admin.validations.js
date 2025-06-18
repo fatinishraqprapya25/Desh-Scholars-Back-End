c318d4176203969e6069394529bdb13915f2fe31
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-18 21:32:55 +0600

    validations for creating admin feature done

---
+++ modules/admin/admin.validations.js
@@ -0,0 +1,27 @@
+// validations/adminValidation.js
+import { z } from 'zod';
+
+const adminValidations = {};
+
+adminValidations.createAdminSchema = z.object({
+    name: z
+        .string({
+            required_error: 'Name is required',
+        })
+        .min(2, { message: 'Name must be at least 2 characters' })
+        .max(100, { message: 'Name is too long' }),
+
+    email: z
+        .string({
+            required_error: 'Email is required',
+        })
+        .email({ message: 'Invalid email address' }),
+
+    password: z
+        .string({
+            required_error: 'Password is required',
+        })
+        .min(6, { message: 'Password must be at least 6 characters' }),
+});
+
+export default adminValidations;
