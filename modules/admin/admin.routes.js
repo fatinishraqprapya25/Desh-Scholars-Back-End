3dddb74baaad4b22d7a972d0af29d55283f6d463
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-18 23:43:22 +0600

    routes & validations setup for admin done

--- modules/admin/admin.routes.js
+++ modules/admin/admin.routes.js
@@ -4,6 +4,7 @@ const adminFeatures = require("./admin.features");
 const { default: adminValidations } = require("./admin.validations");
 
 adminRouter.post("/", validateRequest(adminValidations.createAdminSchema), adminFeatures.createAdmin);
+adminRouter.post("/login", validateRequest(adminValidations.loginAdminSchema), adminFeatures.loginAdmin);
 adminRouter.get("/", adminFeatures.getAllAdmins);
 adminRouter.delete("/:id", adminFeatures.deleteAdmin);
 
--- modules/admin/admin.validations.js
+++ modules/admin/admin.validations.js
@@ -23,4 +23,18 @@ adminValidations.createAdminSchema = z.object({
         .min(6, { message: 'Password must be at least 6 characters' }),
 });
 
+adminValidations.loginAdminSchema = z.object({
+    email: z
+        .string({
+            required_error: 'Email is required',
+        })
+        .email({ message: 'Invalid email address' }),
+
+    password: z
+        .string({
+            required_error: 'Password is required',
+        })
+        .min(6, { message: 'Password must be at least 6 characters' }),
+});
+
 export default adminValidations;
