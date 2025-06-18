ae333fbaea592c09af19b8ed1b493c5b0827ec47
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-19 00:33:42 +0600

    verify admin middleware done

---
+++ middlewares/VerifyAdmin.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const Admin = require("../modules/admin/admin.model");
+const sendResponse = require("../utils/sendResponse");
+
+const verifyAdmin = async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return sendResponse(res, 401, {
+                success: false,
+                message: "Unauthorized: No token provided",
+            });
+        }
+
+        const token = authHeader.split(" ")[1];
+        const decoded = jwt.verify(token, config.jwtSecret);
+
+        const admin = await Admin.findById(decoded.id).select("-password");
+        if (!admin) {
+            return sendResponse(res, 404, {
+                success: false,
+                message: "Admin not found",
+            });
+        }
+
+        req.user = admin;
+        next();
+    } catch (error) {
+        return sendResponse(res, 401, {
+            success: false,
+            message: "Invalid or expired token",
+        });
+    }
+};
+
+module.exports = verifyAdmin;
