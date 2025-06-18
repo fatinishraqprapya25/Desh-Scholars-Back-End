28de00cb6e0362713dfa94c59b7585b1c5d76b21
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-18 23:26:28 +0600

    admin router optmized

--- modules/admin/admin.routes.js
+++ modules/admin/admin.routes.js
@@ -4,6 +4,7 @@ const adminFeatures = require("./admin.features");
 const { default: adminValidations } = require("./admin.validations");
 
 adminRouter.post("/", validateRequest(adminValidations.createAdminSchema), adminFeatures.createAdmin);
+adminRouter.get("/", adminFeatures.getAllAdmins);
 adminRouter.delete("/:id", adminFeatures.deleteAdmin);
 
 module.exports = adminRouter;
