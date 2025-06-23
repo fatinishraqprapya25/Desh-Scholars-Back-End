1b9ca02fa039c6bc371cdb0e39706f829bdd854b
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-23 13:10:39 +0600

    payment model optimized

---
+++ modules/payment/payment.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+    purchaseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    status: {
+        type: String,
+        enum: ['approved', 'pending'],
+        default: 'pending',
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['bkashManual', 'nagadManual'],
+        required: true,
+    },
+    transactionId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Payment', paymentSchema);
