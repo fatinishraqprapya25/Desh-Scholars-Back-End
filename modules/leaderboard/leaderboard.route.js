88a4b28d8c0c0f4778f393d49e54aba61260360c
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2025-06-27 22:30:48 +0600

    routing setup for leaderboard optimzied

--- modules/leaderboard/leaderboard.route.js
+++ modules/leaderboard/leaderboard.route.js
@@ -3,5 +3,6 @@ const leaderboardFeatures = require("./loaderboard.features");
 const leaderboardRouter = require("express").Router();
 
 leaderboardRouter.get("/", leaderboardFeatures.getTestsLeaderBoard);
+leaderboardRouter.get("/:userId", leaderboardFeatures.getUserRank);
 
 module.exports = leaderboardRouter;
